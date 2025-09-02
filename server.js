12fc88b58b7d0a47a4028c2f5e782b6cd2435c2e
pushpaanand <productananlyst.pushpa@kauveryhospital.com>
2025-09-02 13:59:39 +0530

    path issue fix

--- server.js
+++ server.js
@@ -171,7 +171,7 @@ app.post('/api/decrypt', (req, res) => {
 
 
 
-app.get("/*", (req, res) => {
+app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "client/build", "index.html"));
 });
   
